1ce510b75b8f2436dff82b5104be41629bf78c17
Mathesh-kumar <matheshkumar256@gmail.com>
2021-05-26 14:11:03 +0530

    Flipcart review updated version-1

--- main.py
+++ main.py
@@ -25,42 +25,43 @@ def review_scrapping():  # Function to scrap contents
         beautyProductPage = BeautifulSoup(productPage, "html.parser")  # Parse web page as html
 
         allProducts = beautyProductPage.findAll("div", {"class": "_1AtVbE col-12-12"})  # Select all products
-        del allProducts[0:3]  # Delete unwanted contents
-        firstProduct = allProducts[0]  # Select first item only
-        firstProductLink = "https://www.flipkart.com" + firstProduct.div.div.div.a['href']  # Select first item link
+        del allProducts[0:1]  # Delete unwanted contents (Filter option in that page)
+        firstProduct = allProducts[0]  # Select first product from the list of products
+        firstProductLink = "https://www.flipkart.com" + firstProduct.div.div.div.a['href']  # Select first product link
 
-        #  openFirstProduct = urlopen(firstProductLink)  # Request webpage from internet
-        #  firstProductPage = openFirstProduct.read()  # Read content from the page (HTML code)
-        #  openFirstProduct.close()  # Cancel the connection with the web server
-        #  beautyFirstProductPage = BeautifulSoup(firstProductPage, "html.parser")  # Parse web page as html
+        openFirstProduct = urlopen(firstProductLink)  # Request first product webpage from internet
+        firstProductPage = openFirstProduct.read()  # Read content from the page (HTML code)
+        openFirstProduct.close()  # Cancel the connection with the web server
+        beautyFirstProductPage = BeautifulSoup(firstProductPage, "html.parser")  # Parse web page as html
 
-        # Instead of these upcoming two lines we can also use the above four lines
-        firstProductPage = requests.get(firstProductLink)  # Request webpage from internet
-        beautyFirstProductPage = BeautifulSoup(firstProductPage.text, "html.parser")  # Parse web page as html
+        # Instead of above four lines we can also use these upcoming two lines
+        # firstProductPage = requests.get(firstProductLink)  # Request webpage from internet
+        # beautyFirstProductPage = BeautifulSoup(firstProductPage.text, "html.parser")  # Parse web page as html
 
-        commentBoxes = beautyFirstProductPage.findAll("div", {"class": "_16PBlm"})  # Select all comments
+        commentBoxes = beautyFirstProductPage.findAll("div", {"class": "col _2wzgFH"})  # Select all comments
         reviews = []  # List to store the comments
 
         # This for loop will iterate through each comments and retrieve all the information from it.
         # Information line Comment name, rating, heading, review
         for cBox in commentBoxes:
             try:
-                name = cBox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                name = cBox.find_all("p", {"class": "_2sc7ZR _2V5EHH"})[0].text
             except:
                 name = 'No Name'
             try:
-                rating = cBox.div.div.div.div.text
+                rating = cBox.find_all("div", {"class": "_3LWZlK _1BLPMq"})[0].text
             except:
                 rating = 'No Rating'
             try:
-                commentHead = cBox.div.div.div.p.text
+                commentHead = cBox.find_all("p", {"class": "_2-N8zT"})[0].text
             except:
                 commentHead = 'No Comment Heading'
             try:
-                commentTag = cBox.div.div.find_all('div', {'class': ''})
-                customerComment = commentTag[0].div.text
+                customerComment = cBox.find_all("div", {"class": "t-ZTKy"})[0].div.text
+                customerComment = customerComment.replace("READ MORE", "")
             except:
                 customerComment = 'No Customer Comment'
+
             reviewDictionary = dict(Product=productName, Name=name, Rating=rating, CommentHead=commentHead,
                                     Comment=customerComment)  # Store retrieved information as a dictionary
             reviews.append(reviewDictionary)  # Append each comment dictionary into reviews list
